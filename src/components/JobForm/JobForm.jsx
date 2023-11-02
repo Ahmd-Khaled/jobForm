3b8ab094dee98461f57e9991e4666608ff5cbfe2
Ahmd-Khaled <ahmd.khaldd@gmail.com>
2023-11-02 14:05:01 +0200

    v0.4

--- src/components/JobForm/JobForm.jsx
+++ src/components/JobForm/JobForm.jsx
@@ -3,8 +3,12 @@ import Input from "../Input/Input";
 import Select from 'react-select';
 import styles from "./styles.module.scss";
 import { useState } from "react";
+import { format } from 'date-fns';
+import { ClassNames, DayPicker } from 'react-day-picker';
+// import styles from 'react-day-picker/dist/style.module.css';
 
-const availableJobs = [
+
+const jobsList = [
   {
     id: 1,
     label: "مندوب ميداني",
@@ -32,12 +36,41 @@ const availableJobs = [
   },
 ];
 
+const daysList = Array.from(Array(31), (_, index) => ({
+  label: index+1,
+  value: index+1,
+}));
+
+const monthList = Array.from(Array(12), (_, index) => ({
+  label: index+1,
+  value: index+1,
+}));
+
+const yearsList = Array.from(Array(74), (_, index) => ({
+  label: index+1950,
+  value: index+1950,
+}));
+
 const JobForm = () => {
   const [slectedJob, setSlectedJob] = useState("");
+  // const [selected, setSelected] = useState("");
 
-  const slectedJobHandler = (option) => {
-    setSlectedJob(option);
-  }
+  // let footer = <p>Please pick a day.</p>;
+  // if (selected) {
+  //   footer = <p>You picked {format(selected, 'PP')}.</p>;
+  // }
+
+  // const classNames = {
+  //   ...styles,
+  //   head: 'custom-head'
+  // };
+  const [slectedDay, setSlectedDay] = useState("");
+  const [slectedMonth, setSlectedMonth] = useState("");
+  const [slectedYear, setSlectedYear] = useState("");
+
+
+  console.log("slectedJob: ", slectedJob?.value);
+
   return (
     <section className={styles.jobForm}>
       <div className={styles.jobForm__container}>
@@ -52,16 +85,12 @@ const JobForm = () => {
               <div className={styles.inputs}>
                 <Select
                   value={slectedJob}
-                  onChange={slectedJobHandler}
-                  options={availableJobs}
+                  onChange={setSlectedJob}
+                  options={jobsList}
                   className={styles.customSelect}
                   classNamePrefix={styles.innerSelect}
                   placeholder="اختر الوظيفة المطلوبة"
                 />
-                {/* <Select 
-                  defaultOption=""
-                  selectList={availableJobs}
-                /> */}
               </div>
             </div>
             <div className={styles.inputWrapper}>
@@ -84,32 +113,157 @@ const JobForm = () => {
                 />
               </div>
             </div>
+          </div>
+          <div className={styles.group}>
             <div className={styles.inputWrapper}>
               <h3 className={styles.inpTitle}>تاريخ الميلاد</h3>
               <div className={styles.inputs}>
-                <Input 
-                  width="150px"
-                  id="day"
-                  type="text"
+                {/* <DayPicker
+                  mode="single"
+                  selected={selected}
+                  onSelect={setSelected}
+                  footer={footer}
+                  classNames={classNames}
+                /> */}
+                <Select
+                  value={slectedDay}
+                  onChange={setSlectedDay}
+                  options={daysList}
+                  className={styles.customSelect}
+                  classNamePrefix={styles.innerSelect}
                   placeholder="اليوم"
                 />
-                <Input 
-                  width="150px"
-                  id="month"
-                  type="text"
+                <Select
+                  value={slectedMonth}
+                  onChange={setSlectedMonth}
+                  options={monthList}
+                  className={styles.customSelect}
+                  classNamePrefix={styles.innerSelect}
                   placeholder="الشهر"
                 />
-                <Input 
-                  width="150px"
-                  id="year"
-                  type="text"
+                <Select
+                  value={slectedYear}
+                  onChange={setSlectedYear}
+                  options={yearsList}
+                  className={styles.customSelect}
+                  classNamePrefix={styles.innerSelect}
                   placeholder="السنة"
+                />
+              </div>
+            </div>
+            <div className={styles.inputWrapper}>
+              <h3 className={styles.inpTitle}>بيانات محل الميلاد</h3>
+              <div className={styles.inputs}>
+                <Input 
+                  id="placeOfBirth"
+                  type="text"
+                  placeholder="مكان الميلاد"
+                />
+                <Input 
+                  id="nationality"
+                  type="text"
+                  placeholder="الجنسية"
+                />
+                <Input 
+                  id="religion"
+                  type="text"
+                  placeholder="الديانة"
                 />
               </div>
             </div>
           </div>
           <div className={styles.group}>
-            1111
+            <div className={styles.inputWrapper}>
+              <h3 className={styles.inpTitle}>بيانات بطاقة الأحوال المدنية</h3>
+              <div className={styles.inputs}>
+                <Input 
+                  id="placeOfBirth"
+                  type="text"
+                  placeholder="رقم البطاقة / الاقامة"
+                />
+                <Input 
+                  id="nationality"
+                  type="text"
+                  placeholder="مكان الاصدار"
+                />
+                <Input 
+                  id="religion"
+                  type="text"
+                  placeholder="تاريخ الاصدار"
+                />
+              </div>
+            </div>
+            <div className={styles.inputWrapper}>
+              <h3 className={styles.inpTitle}>بيانات جواز السفر</h3>
+              <div className={styles.inputs}>
+                <Input 
+                  id="placeOfBirth"
+                  type="text"
+                  placeholder="رقم الجواز"
+                />
+                <Input 
+                  id="nationality"
+                  type="text"
+                  placeholder="مكان الاصدار"
+                />
+                <Input 
+                  id="religion"
+                  type="text"
+                  placeholder="تاريخ الاصدار"
+                />
+              </div>
+            </div>
+          </div>
+          <div className={styles.group}>
+            <div className={styles.inputWrapper}>
+              <h3 className={styles.inpTitle}>بيانات التواصل</h3>
+              <div className={styles.inputs}>
+                <Input 
+                  id="placeOfBirth"
+                  type="text"
+                  placeholder="هاتف المنزل"
+                />
+                <Input 
+                  id="nationality"
+                  type="text"
+                  placeholder="هاتف العمل"
+                />
+                <Input 
+                  id="religion"
+                  type="text"
+                  placeholder="الجوال"
+                />
+                <Input 
+                  id="religion"
+                  type="email"
+                  placeholder="البريد الالكتروني"
+                />
+              </div>
+            </div>
+            <div className={styles.inputWrapper}>
+              <h3 className={styles.inpTitle}>بيانات العنوان</h3>
+              <div className={styles.inputs}>
+                <Input 
+                  id="placeOfBirth"
+                  type="text"
+                  placeholder="الدولة"
+                />
+                <Input 
+                  id="nationality"
+                  type="text"
+                  placeholder="المدينة"
+                />
+                <Input 
+                  id="religion"
+                  type="text"
+                  placeholder="العنوان الحالي"
+                />
+              </div>
+              <div>
+                <input type="checkbox" id="notInKSA" />
+                <label htmlFor="notInKSA">غير سعودي - هل أنت مستعد لنقل الكفالة</label>
+              </div>
+            </div>
           </div>
         </form>
       </div>
